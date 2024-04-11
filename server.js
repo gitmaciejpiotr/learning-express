7d15502db6a159682e768bb7a1ac99112f1a65db
gitmaciejpiotr <97894371+gitmaciejpiotr@users.noreply.github.com>
2024-04-11 18:42:13 +0200

    Add uploading files feature to contact site

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const multer  = require('multer');
+const upload = multer({ dest: 'public/' });
 const hbs = require('express-handlebars');
 
 const app = express();
@@ -8,6 +10,7 @@ app.engine('.hbs', hbs());
 app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.render('index');
@@ -31,6 +34,22 @@ app.get('/history', (req, res) => {
 
 app.get('/hello/:name', (req, res) => {
     res.render('hello', { layout: false, name: req.params.name });
+});
+
+app.post('/contact/send-message', upload.single('project_design'), (req, res) => {
+
+    const { author, sender, title, message } = req.body;
+
+    console.log(req.file.originalname);
+    console.log(req.body);
+
+    if (author && sender && title && message && req.file.originalname) {
+        res.render('contact', { isSent: true, fileName: req.file.originalname });
+    }
+    else {
+        res.render('contact', { isError: true });
+    }
+
 });
 
 app.use((req, res) => {
